aa318ce62b6322e6f88346faa94de33f0b4f9855
phattranit9302 <55368209+phattranit9302@users.noreply.github.com>
2021-01-28 22:53:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     else if($message == "1"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "คู่มือการใช้งานแอปพลิเคชัน Reduce costs Save material"."\n"."https://docs.google.com/document/d/1CJaSBeO7fPn5N9c0lXvK2z7MOp6qqiL-WqSRVJ24dAg/edit.";
+        $arrayPostData['messages'][0]['text'] = "คู่มือการใช้งานแอปพลิเคชัน Reduce costs Save material"."\n"."https://drive.google.com/file/d/1KyDTqS6kJIGOEuJ6FBOpWUK_MzHyWySU/view?usp=sharing.";
         replyMsg($arrayHeader,$arrayPostData);
     }
     else if($message == "2"){
